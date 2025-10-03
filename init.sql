bfe17c0d0b6261c071ec78c09ce4080c6ec3169b
davidx345 <davidayo2603@gmail.com>
2025-10-03 13:37:24 +0100

    chore: Remove unused EC2 instance, SQL initialization, S3 bucket policy, lifecycle policy, and Vercel configuration files

--- init.sql
+++
@@ -1,8 +0,0 @@
--- Initialize the database
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
--- Create additional indexes for better performance
--- These will be created by Alembic migrations, but we can prepare the database
-
--- Grant necessary permissions
-GRANT ALL PRIVILEGES ON DATABASE solivolt TO postgres;
